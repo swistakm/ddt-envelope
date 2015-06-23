67f54d10216197ae6fd698f9bbb877eb76b60f80
mjaworski <mjaworski@opera.com>
2015-06-23 16:16:47 +0200

    improvements:
    * python3 and new django releses support
    * reformatting of JSON responses
    * fixed issue with default template not shipped with app
    * changed template dir
    * 100% tests coverage

--- ddt_envelope/urls.py
+++ ddt_envelope/urls.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 try:
     from django.conf.urls import url, patterns
-except ImportError:
+except ImportError:  # pragma: no cover
     from django.conf.urls.defaults import url, patterns
 
-import views
+from ddt_envelope import views
 
-urlpatterns = patterns('ddt_envelope.views',
+urlpatterns = patterns(
+    'ddt_envelope.views',
     url(r'^(?P<next>.+)$', views.EnvelopeView.as_view(), name='ddte'),
 )
--- ddt_envelope/views.py
+++ ddt_envelope/views.py
@@ -1,18 +1,52 @@
+import json
+
 from django.views.generic import TemplateView
 from django.core import urlresolvers
 
-class EnvelopeView(TemplateView):
-    template_name = 'ddte/envelope.html'
 
-    def get(self, request, *args, **kwargs):
-        url = "/"+kwargs.get("next")
-        print url
+class EnvelopeView(TemplateView):
+    template_name = 'ddt_envelope/envelope.html'
+
+    def get_target_view_response(self, request, url):
         target_view = urlresolvers.resolve(url)
 
-        view_response = target_view.func(request, **target_view.kwargs)
+        view_response = target_view.func(
+            request,
+            **target_view.kwargs
+        )
         if hasattr(view_response, "render"):
             content = view_response.render()
         else:
             content = view_response
 
-        return self.render_to_response(locals())
+        return content
+
+    def get(self, request, *args, **kwargs):
+        url = "/" + kwargs.get("next")
+        target_response = self.get_target_view_response(request, url)
+
+        if self.is_json(target_response):
+            content = self.reformat_json(target_response.content)
+        else:
+            content = target_response.content
+
+        return self.render_to_response({
+            'url': url,
+            'content': content,
+            'response': target_response,
+            'headers': dict([
+                (header, value) for header, value in target_response.items()
+            ]),
+        })
+
+    def is_json(self, response):
+        return response.get('Content-Type') == 'application/json'
+
+    def reformat_json(self, content):
+        # note: in python3 response content might be bytes so we must decode it
+        if isinstance(content, bytes):
+            raw_content = content.decode('utf-8')
+        else:
+            raw_content = content
+
+        return json.dumps(json.loads(raw_content), indent=4)
